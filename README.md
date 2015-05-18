727de7fde42e61106351a14be015775e03758869
Brandon Smith <vectorcell@gmail.com>
2015-05-18 04:00:06 -0500

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tmux-configuration
+
 A set of configuration files and scripts for tmux.
+
+tmux.conf should be linked to as ~/.tmux.conf
+all tmux-* scripts should be in the user's path
+
+The install.sh script will place a link to tmux.conf in the user's home directory, and the scripts starting with "tmux-" will be linked to in the user's private bin directory, if it exists at ~/bin.
---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+echo "This script should be run from the directory where it resides."
+
+echo "This script will overwrite the following files (if they exist):"
+echo -e "\t~/.tmux.conf"
+for file in $(ls | grep "tmux-"); do
+	echo -e "\t~/bin/$file"
+done
+
+while true; do
+read -p "Do you wish to install this program? [y/n]:" yn
+	case $yn in
+		[Yy]* ) break;;
+		[Nn]* ) echo "not installing, exiting ..."; exit;;
+		* ) echo "Please answer yes or no.";;
+	esac
+done
+
+echo "Installing ..."
+if [ -e "$HOME/.tmux.conf" ]; then
+	echo -e "\nremoving $HOME/.tmux.conf"
+	rm "$HOME/.tmux.conf"
+fi
+echo -e "linking $HOME/.tmux.conf to $DIR/tmux.conf"
+ln -s $DIR/tmux.conf $HOME/.tmux.conf
+for file in $(ls | grep "tmux-"); do
+	if [ -e "$HOME/bin/$file" ]; then
+
+		echo -e "\nremoving $HOME/bin/$file"
+		rm "$HOME/bin/$file"
+	fi
+	echo -e "linking $HOME/bin/$file to $DIR/bin/$file"
+	ln -s $DIR/$file $HOME/bin/$file
+done
+
+tmux refresh-client -S
